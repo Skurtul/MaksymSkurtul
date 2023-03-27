cf4ab00ef584d751dd3a82f29d81410053f6af6d
Maksym Skurtul <mskurtul@gmail.com>
2023-03-27 15:27:39 +0300

    added introduction js

---
+++ lesson29__introduction-js-min/scripts/script.js
@@ -0,0 +1,14 @@
+let currentUser;
+let currentVisitor;
+let currentClient;
+let userName;
+
+// let userA;
+// let userNumber;
+// let currentuser;
+// let CURRENTUSER;
+// let user;
+
+/* Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit harum nobis quo magni dicta praesentium delectus sunt, perspiciatis dolorum illum, deleniti quibusdam optio quasi inventore in, similique eveniet accusamus neque.
+Voluptatum, pariatur assumenda quisquam iure sint nisi expedita. Accusamus, explicabo officia impedit quod totam, aliquid minima, officiis blanditiis in enim consectetur eaque quis sapiente odio alias magnam ipsam ipsum deserunt!
+Impedit hic praesentium et voluptate in! Mollitia molestias ab, dolore officiis quos qui possimus harum quasi corrupti quod, veniam amet, itaque doloribus? Tenetur dicta natus, sapiente voluptatibus corporis sunt beatae! */
---
+++ lesson29__introduction-js-normal-max/script.js
@@ -0,0 +1,30 @@
+"use sctrict";
+
+/* let userName = prompt("What is your name?", "");
+alert(`Hello, ${userName}!`); */
+
+/* const yearNow = 2023;
+let birthYearUser = prompt("What year were you born?", "");
+let ageUser = yearNow - birthYearUser;
+
+if (birthYearUser == null) alert(`Year of birth is not given`);
+else alert(`Your ${ageUser}`); */
+
+/* let sideLength = prompt("What is the length of the side of the square?", "");
+let squarePerimeter = Math.pow(sideLength, 2);
+alert(`The perimeter of your square is: ${squarePerimeter}`); */
+
+/* let circleRadius = prompt("What is your circle radius?", "");
+let circleArea = Math.PI * Math.pow(circleRadius, 2);
+alert(`The area of your circle: ${circleArea}`); */
+
+/* let distance = prompt("What is the distance in km from point A to point B?", '');
+let travelTime = prompt("How many hours do you want to spend on the road?", '');
+let averageSpeed = (distance/travelTime);
+alert (`The average speed should be: ${averageSpeed} km/h`); */
+
+/* const EUR = 0.93;
+const PLN = 4.36;
+const UAH = 36.96;
+let USD = prompt("Enter value in USD:", "");
+alert(`You'll get: ${USD * EUR} EUR, ${USD * PLN} PLN, ${USD * UAH} UAH.`); */
