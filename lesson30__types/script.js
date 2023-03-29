c56056e964933a3f3f9f8446a9cd1ae740dd0383
Maksym Skurtul <mskurtul@gmail.com>
2023-03-29 14:53:17 +0300

    added lesson__30

---
+++ lesson30__types/script.js
@@ -0,0 +1,32 @@
+"use strict";
+
+let numberFirst = 0.1;
+let numberSecond = 0.2;
+let sum = numberFirst + numberSecond;
+alert(sum.toFixed(1));
+
+/* let string = "1";
+let number = 2;
+let sum = +string + number;
+alert(sum); */
+
+/* const storageFile = 820;
+let storageSpace = prompt("How is storage space(GB) ?", '');
+const num = parseFloat(storageSpace);
+alert(`You will be able to store: ${Math.floor(num * 1000 / storageFile)} files of 820 MB.`); */
+
+/* let sum = prompt("How much do you have?", '');
+let candyPrice = prompt("How much is a candy bar?", '');
+let amountСhocolates = parseFloat(sum) / parseFloat(candyPrice);
+let change = parseFloat(sum) - parseFloat(candyPrice) * Math.floor(amountСhocolates);
+alert(`You can buy: ${Math.floor(amountСhocolates)}, change: ${change.toFixed(2)}`); */
+
+/* let depositAmount = prompt("How much of your deposit?", '');
+let months = prompt("For how many months you make a deposit?", '') 
+const interest = 5;
+let result = (depositAmount / 100 * (interest / 12 * months))
+alert (`Your income will be: ${result.toFixed(2)}`); */
+
+// 2 && 0 && 3; // 0;
+// 2 || 0 || 3; // 2;
+// 2 && 0 || 3; // 3;
