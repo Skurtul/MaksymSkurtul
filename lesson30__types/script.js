1d48acb5b6fef7c2154d26f60abccb30051553d3
Maksym Skurtul <mskurtul@gmail.com>
2023-03-30 15:29:39 +0300

    parse prompt and deposit calculate

--- lesson30__types/script.js
+++ lesson30__types/script.js
@@ -1,9 +1,9 @@
 "use strict";
 
-let numberFirst = 0.1;
+/* let numberFirst = 0.1;
 let numberSecond = 0.2;
 let sum = numberFirst + numberSecond;
-alert(sum.toFixed(1));
+alert(sum.toFixed(1)); */
 
 /* let string = "1";
 let number = 2;
@@ -11,20 +11,19 @@ let sum = +string + number;
 alert(sum); */
 
 /* const storageFile = 820;
-let storageSpace = prompt("How is storage space(GB) ?", '');
-const num = parseFloat(storageSpace);
-alert(`You will be able to store: ${Math.floor(num * 1000 / storageFile)} files of 820 MB.`); */
+let storageSpace = parseFloat(prompt("How is storage space(GB) ?", ''));
+alert(`You will be able to store: ${Math.floor(storageSpace * 1000 / storageFile)} files of 820 MB.`); */
 
-/* let sum = prompt("How much do you have?", '');
-let candyPrice = prompt("How much is a candy bar?", '');
-let amountСhocolates = parseFloat(sum) / parseFloat(candyPrice);
-let change = parseFloat(sum) - parseFloat(candyPrice) * Math.floor(amountСhocolates);
+/* let sum = parseFloat(prompt("How much do you have?", ''));
+let candyPrice = parseFloat(prompt("How much is a candy bar?", ''));
+let amountСhocolates = sum / candyPrice;
+let change = sum - candyPrice * Math.floor(amountСhocolates);
 alert(`You can buy: ${Math.floor(amountСhocolates)}, change: ${change.toFixed(2)}`); */
 
 /* let depositAmount = prompt("How much of your deposit?", '');
 let months = prompt("For how many months you make a deposit?", '') 
-const interest = 5;
-let result = (depositAmount / 100 * (interest / 12 * months))
+const interest = 0.05;
+let result = depositAmount * (1 + interest) ** (months / 12) - depositAmount;
 alert (`Your income will be: ${result.toFixed(2)}`); */
 
 // 2 && 0 && 3; // 0;
